944d942f20f2eeaf5c7a0cb3a02907244f9e2567
Mohamed Abusaif <67015050+Mohamed-Abusaif@users.noreply.github.com>
2023-06-05 23:18:52 +0300

    change

--- server.py
+++ server.py
@@ -15,10 +15,33 @@ def recommend():
     data = request.json
     user_interests = data["interests"]
     recommended_courses = RS.recommend_courses(user_interests, df)
-    print(recommended_courses)
-    return recommended_courses.to_json()
+
+    # Combine similar keys in each object into a new object
+    combined_recommendations = {}
+    for index, row in recommended_courses.iterrows():
+        course_id = row['Course ID']
+        if course_id not in combined_recommendations:
+            combined_recommendations[course_id] = {
+                'Course ID': course_id,
+                'Course Name': row['Course Name'],
+                'University': row['University'],
+                'Difficulty Level': [],
+                'Rating': [],
+                'URL': [],
+                'Description': []
+            }
+        combined_recommendations[course_id]['Difficulty Level'].append(row['Difficulty Level'])
+        combined_recommendations[course_id]['Rating'].append(row['Rating'])
+        combined_recommendations[course_id]['URL'].append(row['URL'])
+        combined_recommendations[course_id]['Description'].append(row['Description'])
+
+    # Create a list of the combined recommendations
+    final_recommendations = list(combined_recommendations.values())
+
+    print(final_recommendations)
+    return jsonify(final_recommendations)
 
 
-# start the Flask app
+# Start the Flask app
 if __name__ == '__main__':
     app.run(debug=True)
